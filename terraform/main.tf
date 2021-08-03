64c32c1c351a457b82fd6f6c843859a0c578dbd9
Mahendra Singh <mahendra.singh@oodlestechnologies.com>
2021-08-03 17:56:23 +0530

    Intial commit

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAUQVEH7G3SH4F4TO2"
+  secret_key = "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"
+}
+
+resource "aws_instance" "ansible_tf" {
+      
+	ami = "ami-0747bdcabd34c712a"
+    instance_type = "t2.micro"
+    key_name = "ritesh-ec2"
+	security_groups = [ "wizard-SG1" ]
+	tags = {
+		name = "ansible"
+	}
+
+	provisioner "local-exec" {
+		command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=false ansible-playbook --private-key /home/ms/Documents/ritesh-ec2.pem -i ${aws_instance.ansible_tf.public_ip}, playbook.yaml"
+	  
+	}
+}
